59de078cfd2411d98e8518650c2d8248aac1f192
skup5 <5skup5@seznam.cz>
2017-06-24 16:41:37 +0200

    MainActivity + received sms log

---
+++ android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/MainActivity.java
@@ -0,0 +1,191 @@
+package cz.zelenikr.remotetouch;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final Uri SMS_INBOX = Uri.parse("content://sms/inbox");
+  private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+  private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 2;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         String permissions[], int[] grantResults) {
+    switch (requestCode) {
+      case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+          // permission was granted, yay! Do the
+          // contacts-related task you need to do.
+
+        } else {
+
+          // permission denied, boo! Disable the
+          // functionality that depends on this permission.
+        }
+        return;
+      }
+      case MY_PERMISSIONS_REQUEST_READ_SMS: {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+          onSmsBtClick(findViewById(R.id.smsBt));
+        } else {
+
+          // permission denied, boo! Disable the
+          // functionality that depends on this permission.
+        }
+        return;
+      }
+
+      // other 'case' lines to check for other
+      // permissions this app might request
+    }
+  }
+
+  public void onCallsBtClick(View view){
+
+  }
+
+  public void onSmsBtClick(View view){
+    if(!checkSmsPermission()) return;
+
+    ListView list = (ListView) findViewById(R.id.listView);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+    Cursor cursor = getContentResolver().query(SMS_INBOX, new String[] {"body", "address"},
+        null, null, "date desc limit 3");
+    String message = "";
+
+    while (cursor.moveToNext()){
+      message = "";
+      message+="tel.:"+cursor.getString(1)+"\n";
+      message+="content:"+cursor.getString(0)+"\n";
+      adapter.add(message);
+    }
+    list.setAdapter(adapter);
+  }
+
+  private boolean checkSmsPermission() {
+    // Here, thisActivity is the current activity
+    if (ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_SMS)
+        != PackageManager.PERMISSION_GRANTED) {
+
+      // Should we show an explanation?
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+          Manifest.permission.READ_SMS)) {
+
+        // Show an explanation to the user *asynchronously* -- don't block
+        // this thread waiting for the user's response! After the user
+        // sees the explanation, try again to request the permission.
+
+        Log.i(getLocalClassName(), "chechSmsPermission(): shouldShowRequestPermissionRationale");
+      } else {
+
+        // No explanation needed, we can request the permission.
+
+        ActivityCompat.requestPermissions(this,
+            new String[]{Manifest.permission.READ_SMS},
+            MY_PERMISSIONS_REQUEST_READ_SMS);
+
+        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
+        Log.i(getLocalClassName(), "checkSmsPermission: request permission");
+      }
+      return false;
+    }
+    Log.i(getLocalClassName(), "checkSmsPermission: permission granted");
+    return true;
+  }
+
+  private void checkContactsPermission() {
+    // Here, thisActivity is the current activity
+    if (ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_CONTACTS)
+        != PackageManager.PERMISSION_GRANTED) {
+
+      // Should we show an explanation?
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+          Manifest.permission.READ_CONTACTS)) {
+
+        // Show an explanation to the user *asynchronously* -- don't block
+        // this thread waiting for the user's response! After the user
+        // sees the explanation, try again to request the permission.
+
+      } else {
+
+        // No explanation needed, we can request the permission.
+
+        ActivityCompat.requestPermissions(this,
+            new String[]{Manifest.permission.READ_CONTACTS},
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
+      }
+    }
+  }
+}
