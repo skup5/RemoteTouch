f9f256b80a54c22111035afdaba747ac8117ba03
skup5 <5skup5@seznam.cz>
2017-08-10 14:20:34 +0200

    created Xamarin solution with Android,iOS projects

---
+++ xamarin/RemoteTouch/RemoteTouch/RemoteTouch/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RemoteTouch
+{
+	public partial class MainPage : ContentPage
+	{
+        private int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private  void OnButtonClicked(object sender, EventArgs args)
+        {
+            count++;
+            label.Text = count + "x clicked";
+        }
+	}
+}
