91a0084fd86223c55170bdaf289864b19e42351a
skup5 <5skup5@seznam.cz>
2017-11-09 22:58:58 +0100

    Android - NotificationHandler was fixed

--- xamarin/RemoteTouch/RemoteTouch/RemoteTouch.Android/MainActivity.cs
+++ xamarin/RemoteTouch/RemoteTouch/RemoteTouch.Android/MainActivity.cs
@@ -1,30 +1,99 @@
 ﻿using System;
+using System.Text;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Util;
-using CrossLibrary;
+using Android.Content;
+using Android.Provider;
+using Android.Support.V4.Content;
+using Android.Text;
 
 namespace RemoteTouch.Droid
 {
     [Activity(Label = "@string/AppName", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        private const string ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
+        private NotificationHandlerReceiver _notificationHandlerReceiver;
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new RemoteTouch.App());
 
             Log.Info("Msg", "Start MainActivity");
-            System.Console.WriteLine("Start MainActivity");
+            Console.WriteLine("Start MainActivity");
+
+            _notificationHandlerReceiver = new NotificationHandlerReceiver();
+            LocalBroadcastManager.GetInstance(this).RegisterReceiver(_notificationHandlerReceiver,
+                new IntentFilter("cz.zelenikr.remotetouch.notificationhandler"));
+
+            EnableNotificationService();
+        }
+
+
+        private bool IsNotificationServiceEnabled()
+        {
+            String pkgName = PackageName;
+            string flat = Settings.Secure.GetString(ContentResolver,
+                ENABLED_NOTIFICATION_LISTENERS);
+            if (!TextUtils.IsEmpty(flat))
+            {
+                string[] names = flat.Split(':');
+                for (int i = 0; i < names.Length; i++)
+                {
+                    ComponentName cn = ComponentName.UnflattenFromString(names[i]);
+                    if (cn != null)
+                    {
+                        if (TextUtils.Equals(pkgName, cn.PackageName))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void EnableNotificationService()
+        {
+            if (!IsNotificationServiceEnabled())
+            {
+                new Android.Support.V7.App.AlertDialog.Builder(this)
+                    .SetIcon(Resource.Drawable.Icon)
+                    .SetTitle(Resource.String.AppName)
+                    .SetMessage("Je nutné povolit oprávnění. Přejete si otevřít příslušné nastavení?")
+                    .SetPositiveButton("Ano",
+                        (sender, args) =>
+                            StartActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")))
+                    .SetNegativeButton("Ne", (sender, args) => { })
+                    .Show();
+            }
+        }
+    }
+
+
+    class NotificationHandlerReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Log.Info(GetType().Name, "OnReceive()");
+            Console.WriteLine("NotificationHandlerReceiver.OnReceive()");
+
+            StringBuilder msg = new StringBuilder("NotificationHandler:\n");
+            msg.AppendLine(intent.GetStringExtra("package"))
+                .AppendLine(intent.GetStringExtra("ticker"))
+                .AppendLine(intent.GetStringExtra("title"))
+                .AppendLine(intent.GetStringExtra("text"));
+
+            Toast.MakeText(context, msg.ToString(), ToastLength.Long).Show();
         }
     }
 }
--- xamarin/RemoteTouch/RemoteTouch/RemoteTouch.Android/NotificationHandler.cs
+++ xamarin/RemoteTouch/RemoteTouch/RemoteTouch.Android/NotificationHandler.cs
@@ -1,55 +1,63 @@
-﻿using Android.Content;
+﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Service.Notification;
+using Android.Support.V4.Content;
 using Android.Util;
 
 namespace RemoteTouch.Droid
 {
+    [Service(Label = "RemoteTouch", Permission = "android.permission.BIND_NOTIFICATION_LISTENER_SERVICE")]
+    [IntentFilter(new[] {"android.service.notification.NotificationListenerService"})]
     public class NotificationHandler : NotificationListenerService
     {
         private Context _context;
 
-        public Context Context { get => _context; set => _context = value; }
+        public Context Context
+        {
+            get => _context;
+            set => _context = value;
+        }
 
         public override void OnCreate()
         {
             base.OnCreate();
-            Context = base.ApplicationContext;
+            Context = ApplicationContext;
             System.Console.WriteLine("Start NotificationHandler");
         }
 
         public override void OnNotificationPosted(StatusBarNotification sbn)
         {
             string pack = sbn.PackageName;
-            string ticker = sbn.Notification.TickerText.ToString();
+            string ticker = sbn.Notification.TickerText != null ? sbn.Notification.TickerText.ToString() : "null";
             Bundle extras = sbn.Notification.Extras;
             string title = extras.GetString("android.title");
-            string text = extras.GetCharSequence("android.text").ToString();
+            string text = extras.GetCharSequence("android.text");
 
             Log.Info("Package", pack);
             Log.Info("Ticker", ticker);
             Log.Info("Title", title);
             Log.Info("Text", text);
 
-            System.Console.WriteLine("Package: "+ pack);
-            System.Console.WriteLine("Ticker: "+ ticker);
-            System.Console.WriteLine("Title: "+ title);
-            System.Console.WriteLine("Text: "+ text);
+            System.Console.WriteLine("Package: " + pack);
+            System.Console.WriteLine("Ticker: " + ticker);
+            System.Console.WriteLine("Title: " + title);
+            System.Console.WriteLine("Text: " + text);
 
             // Create Intent for BroadcastReceiver(s)
-            //Intent msgrcv = new Intent("Msg");
-            //msgrcv.PutExtra("package", pack);
-            //msgrcv.PutExtra("ticker", ticker);
-            //msgrcv.PutExtra("title", title);
-            //msgrcv.PutExtra("text", text);
+            Intent msgrcv = new Intent("cz.zelenikr.remotetouch.notificationhandler");
+            msgrcv.PutExtra("package", pack);
+            msgrcv.PutExtra("ticker", ticker);
+            msgrcv.PutExtra("title", title);
+            msgrcv.PutExtra("text", text);
 
             // Send broadcast
-            //LocalBroadcastManager.getInstance(Context).sendBroadcast(msgrcv);
+            LocalBroadcastManager.GetInstance(Context).SendBroadcast(msgrcv);
         }
 
         public override void OnNotificationRemoved(StatusBarNotification sbn)
         {
-            Log.Info("Msg", "Notification Removed");
+            Log.Info(GetType().Name, "Notification Removed");
             System.Console.WriteLine("Notification Removed");
         }
     }
