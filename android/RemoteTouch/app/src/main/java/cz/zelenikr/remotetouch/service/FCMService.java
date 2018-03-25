35702081ed57adbd382a3f63d23546787fbee211
skup5 <5skup5@seznam.cz>
2018-03-25 14:45:05 +0200

    FCM: receiving broadcasts

--- android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/service/FCMService.java
+++ android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/service/FCMService.java
@@ -24,100 +24,108 @@ import cz.zelenikr.remotetouch.MainActivity;
  */
 public class FCMService extends FirebaseMessagingService {
 
-  private static final String TAG = FCMService.class.getSimpleName();
+    private static final String TAG = FCMService.class.getSimpleName();
 
-  /**
-   * Called when message is received.
-   *
-   * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-   */
-  // [START receive_message]
-  @Override
-  public void onMessageReceived(RemoteMessage remoteMessage) {
-    // [START_EXCLUDE]
-    // There are two types of messages data messages and notification messages. Data messages are handled
-    // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-    // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-    // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-    // When the user taps on the notification they are returned to the app. Messages containing both notification
-    // and data payloads are treated as notification messages. The Firebase console always sends notification
-    // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-    // [END_EXCLUDE]
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
 
-    // TODO(developer): Handle FCM messages here.
-    // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-    Log.d(TAG, "From: " + remoteMessage.getFrom());
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-    // Check if message contains a data payload.
-    if (remoteMessage.getData().size() > 0) {
-      Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-      if (/* Check if data needs to be processed by long running job */ true) {
-        // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-        scheduleJob();
-      } else {
-        // Handle message within 10 seconds
-        handleNow();
-      }
+            if (/* Check if data needs to be processed by long running job */ false) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow(remoteMessage);
+            }
 
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
+    // [END receive_message]
 
-    // Check if message contains a notification payload.
-    if (remoteMessage.getNotification() != null) {
-      Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-    }
-
-    // Also if you intend on generating your own notifications as a result of a received FCM
-    // message, here is where that should be initiated. See sendNotification method below.
-  }
-  // [END receive_message]
-
-  /**
-   * Schedule a job using FirebaseJobDispatcher.
-   */
-  private void scheduleJob() {
-    // [START dispatch_job]
+    /**
+     * Schedule a job using FirebaseJobDispatcher.
+     */
+    private void scheduleJob() {
+        // [START dispatch_job]
 //    FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
 //    Job myJob = dispatcher.newJobBuilder()
 //            .setService(MyJobService.class)
 //            .setTag("my-job-tag")
 //            .build();
 //    dispatcher.schedule(myJob);
-    // [END dispatch_job]
-  }
+        // [END dispatch_job]
+    }
 
-  /**
-   * Handle time allotted to BroadcastReceivers.
-   */
-  private void handleNow() {
-    Log.d(TAG, "Short lived task is done.");
-  }
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private void handleNow(RemoteMessage remoteMessage) {
+        Log.i(TAG, "handleNow");
+        String cmd = remoteMessage.getData().get("cmd");
+        if (cmd != null) switch (cmd) {
+            case "SIGN_UP":
+                Log.i(TAG, "handleNow: " + cmd);
+                break;
+            default:
+                break;
+        }
+    }
 
-  /**
-   * Create and show a simple notification containing the received FCM message.
-   *
-   * @param messageBody FCM message body received.
-   */
-  private void sendNotification(String messageBody) {
-    Intent intent = new Intent(this, MainActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
             PendingIntent.FLAG_ONE_SHOT);
 
 
-    Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-    Notification.Builder notificationBuilder =
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification.Builder notificationBuilder =
             new Notification.Builder(this)
 //                    .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                    .setContentTitle("FCM Message")
-                    .setContentText(messageBody)
-                    .setAutoCancel(true)
-                    .setSound(defaultSoundUri)
-                    .setContentIntent(pendingIntent);
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
 
-    NotificationManager notificationManager =
+        NotificationManager notificationManager =
             (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-    notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-  }
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
 }
