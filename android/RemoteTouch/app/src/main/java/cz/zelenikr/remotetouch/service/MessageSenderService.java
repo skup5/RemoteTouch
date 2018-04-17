5ed58e931c5c83865aad4a7fb83506edaa08efdc
skup5 <5skup5@seznam.cz>
2018-04-17 14:09:31 +0200

    MessageSenderService resent X times msg if error occures.

--- android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/service/MessageSenderService.java
+++ android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/service/MessageSenderService.java
@@ -43,8 +43,9 @@ public class MessageSenderService extends Service implements SharedPreferences.O
 
     private static final int ONGOING_NOTIFICATION_ID = 1;
     private static final String TAG = MessageSenderService.class.getSimpleName();
+    private static final int RESENT_MSG_ATTEMPTS = 3, RESENT_MSG_DELAY = 5000;
     private Looper serviceLooper;
-    private MessageHandler messageHandler;
+    private MessageHandler serviceMessageHandler;
     private SecureRestClient restClient;
 
     // TODO: 29.3.2018 Queue for unsent messages
@@ -71,7 +72,7 @@ public class MessageSenderService extends Service implements SharedPreferences.O
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null && intent.getBooleanExtra(INTENT_EXTRA_IS_MSG, false)) {
-            messageHandler.obtainMessage(0, intent).sendToTarget();
+            serviceMessageHandler.obtainMessage(0, intent).sendToTarget();
         }
 
         // Restart after kill
@@ -110,6 +111,13 @@ public class MessageSenderService extends Service implements SharedPreferences.O
         }
     }
 
+    /**
+     * Initializes {@link SecureRestClient} using the specific client token, server url and encryption key.
+     *
+     * @param token the given client identification token
+     * @param url   the given  base rest server url
+     * @param key   the given encryption key
+     */
     private void initRestClient(String token, String url, String key) {
         try {
 //      this.restClient = new JsonSimpleRestClient(loadClientToken(), new URL(loadRestUrl()));
@@ -128,24 +136,45 @@ public class MessageSenderService extends Service implements SharedPreferences.O
     }
 
     private void initServiceHandler(Looper looper) {
-        messageHandler = new MessageHandler(looper);
+        serviceMessageHandler = new MessageHandler(looper);
+        serviceMessageHandler.setAttempts(RESENT_MSG_ATTEMPTS);
+        serviceMessageHandler.setDelay(RESENT_MSG_DELAY);
     }
 
+    /**
+     * Reads client token from device store.
+     *
+     * @return client token or empty string if error occurs
+     */
     private String loadClientToken() {
         return SettingsHelper.getToken(this);
 //        return "1";
     }
 
+    /**
+     * Reads rest server url from device store.
+     *
+     * @return url or empty string if error occurs
+     */
     private String loadRestUrl() {
         return SettingsHelper.getServerUrl(this);
     }
 
+    /**
+     * Reads secure (encryption) key from device store.
+     *
+     * @return key or empty string if error occurs
+     */
     private String loadSecureKey() {
         String key = SettingsHelper.getPairKey(this);
         if (key.isEmpty()) Log.e(TAG, "loadSecureKey: key is missing");
         return key;
     }
 
+    /**
+     * Make this service run in the foreground, supplying the ongoing notification
+     * to be shown to the user while in this state.
+     */
     private void showNotification() {
         Intent notificationIntent = new Intent(this, NavigationActivity.class);
         PendingIntent pendingIntent =
@@ -166,6 +195,9 @@ public class MessageSenderService extends Service implements SharedPreferences.O
         startForeground(ONGOING_NOTIFICATION_ID, builder.build());
     }
 
+    /**
+     * @return true if device is connected to the network
+     */
     private boolean isConnected() {
         return ConnectionHelper.isConnected(this);
     }
@@ -179,7 +211,25 @@ public class MessageSenderService extends Service implements SharedPreferences.O
     }
 
 
+    /**
+     * This {@link Handler} processes intents received by {@link MessageSenderService} service.
+     */
     private final class MessageHandler extends Handler {
+
+        /**
+         * millis delay for message resent
+         */
+        private long delay = 0;
+
+        /**
+         * current processed message
+         */
+        private Message currentMsg;
+
+        /**
+         * number of attempts to sent message
+         */
+        private int attempts = 1;
 
         public MessageHandler(Looper looper) {
             super(looper);
@@ -187,33 +237,86 @@ public class MessageSenderService extends Service implements SharedPreferences.O
 
         @Override
         public void handleMessage(Message msg) {
+            currentMsg = msg;
+
+            // is new msg, set number of attempts
+            if (msg.arg1 == 0) msg.arg1 = getAttempts();
+
+            // msg has right content
             if (msg.obj instanceof Intent) {
                 final Intent intent = (Intent) msg.obj;
                 final Serializable serializableExtra = intent.getSerializableExtra(INTENT_EXTRA_NAME);
+                // data is missing
                 if (serializableExtra == null) {
                     Log.w(TAG, "DTO is null");
                     return;
                 }
+                // valid data type
                 if (serializableExtra instanceof EventDTO) {
                     handleEvent((EventDTO) serializableExtra);
                 } else {
                     Log.w(TAG, serializableExtra.getClass().getSimpleName() + " isn't available DTO instance.");
                 }
             }
-
-            // Stop the service using the startId, so that we don't stop
-            // the service in the middle of handling another job
-            //stopSelf(msg.arg1);
         }
 
+        public int getAttempts() {
+            return attempts;
+        }
+
+        /**
+         * Sets message resent attempts.
+         *
+         * @param attempts should be greater then 0
+         */
+        public void setAttempts(int attempts) {
+            if (attempts <= 0)
+                throw new IllegalArgumentException("Value has to be greater then 0");
+
+            this.attempts = attempts;
+        }
+
+        public long getDelay() {
+            return delay;
+        }
+
+        /**
+         * Sets delay for message resent.
+         *
+         * @param delay (milliseconds) should be positive digit or zero
+         */
+        public void setDelay(long delay) {
+            if (delay < 0)
+                throw new IllegalArgumentException("Value can be only positive digit or zero.");
+
+            this.delay = delay;
+        }
+
+        /**
+         * Processes the specific {@link EventDTO} and sends it to the server.
+         *
+         * @param event the given event
+         */
         private void handleEvent(EventDTO event) {
             Log.i(TAG, "received event " + event.getType().name() + ": " + event.getContent().toString());
             if (isConnected()) {
-                boolean result = restClient.send(event, EVENT_REST_PATH);
+                // message has not been sent
+                if (!restClient.send(event, EVENT_REST_PATH)) {
+                    // msg has enough number of resent attempts
+                    if (currentMsg.arg1 - 1 > 0) {
+                        currentMsg.arg1--;
+                        // return msg into the message queue
+                        sendMessageDelayed(copyOf(currentMsg), getDelay());
+                        Log.i(TAG, "Resent EVENT after " + getDelay() / 1000 + "s");
+                    }
+                }
             } else {
                 Log.i(TAG, "handleEvent: device is not connected to network");
             }
         }
 
+        private Message copyOf(Message msg){
+            return obtainMessage(msg.what, msg.arg1, msg.arg2, msg.obj);
+        }
     }
 }
