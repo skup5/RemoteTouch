32005ab6f0c20350d7e4d55f9ed447c344d068f1
skup5 <5skup5@seznam.cz>
2018-05-27 19:12:52 +0200

    Improved installed apps list (user apps only).

--- android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/helper/AndroidAppHelper.java
+++ android/RemoteTouch/app/src/main/java/cz/zelenikr/remotetouch/helper/AndroidAppHelper.java
@@ -1,9 +1,12 @@
 package cz.zelenikr.remotetouch.helper;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
@@ -19,18 +22,20 @@ import cz.zelenikr.remotetouch.data.AppInfo;
 /**
  * The code was got from
  * <a href='https://www.davebennett.tech/android-recyclerview-for-installed-apps-with-checkbox/'>
- * Android Recyclerview for Installed Apps with Checkbox</a>
+ * Android Recyclerview for Installed Apps with Checkbox</a> and
+ * <a href='https://stackoverflow.com/a/30446616'>Stack Overflow</a>
  */
 public final class AndroidAppHelper {
 
     /**
-     * Returns sorted list (by name) of installed apps.
+     * Returns sorted list (by name) of installed apps. List should contains only user applications
+     * without system services.
      *
      * @param context
      * @return
      */
     public static List<AppInfo> getApps(@NonNull Context context) {
-        return loadApps(context);
+        return loadUserApps(context);
     }
 
     /**
@@ -93,12 +98,12 @@ public final class AndroidAppHelper {
     }
 
     /**
-     * Loads all installed apps into the {@code installedApps} list. List is sorted by app name.
+     * Loads and returns all installed (user and system) apps. The apps list is sorted by the app name.
      *
      * @param context
-     * @return installed apps
+     * @return sorted installed apps list
      */
-    private static List<AppInfo> loadApps(Context context) {
+    private static List<AppInfo> loadAllApps(Context context) {
         AppInfo newApp;
         boolean userApp;
         List<ApplicationInfo> packages = context.getPackageManager().getInstalledApplications(0);
@@ -112,6 +117,37 @@ public final class AndroidAppHelper {
             userApp = (packageInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1;
             newApp.setSystem(!userApp);
             installedApps.add(newApp);
+        }
+
+        Collections.sort(installedApps, (s1, s2) -> s1.getAppName().compareToIgnoreCase(s2.getAppName()));
+        return installedApps;
+    }
+
+    /**
+     * Loads and returns all user (launchable) installed apps. The apps list is sorted by the app name.
+     *
+     * @param context
+     * @return sorted installed apps list
+     */
+    private static List<AppInfo> loadUserApps(Context context) {
+        PackageManager pm = context.getPackageManager();
+        Intent main = new Intent(Intent.ACTION_MAIN, null);
+        main.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> launchables = pm.queryIntentActivities(main, 0);
+        List<AppInfo> installedApps = new ArrayList<>(launchables.size());
+        AppInfo newApp;
+        ActivityInfo activityInfo;
+
+        for (ResolveInfo resolveInfo : launchables) {
+            activityInfo = resolveInfo.activityInfo;
+            if (activityInfo != null) {
+                newApp = new AppInfo();
+                newApp.setAppName(resolveInfo.loadLabel(pm).toString());
+                newApp.setAppPackage(activityInfo.packageName);
+                newApp.setAppIcon(resolveInfo.loadIcon(pm));
+                newApp.setSystem(false);
+                installedApps.add(newApp);
+            }
         }
 
         Collections.sort(installedApps, (s1, s2) -> s1.getAppName().compareToIgnoreCase(s2.getAppName()));
